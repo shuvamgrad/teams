c567f11cf4959d8bf1a19b39cd96746709788777
shuvamgrad <shuvam15nitt@gmail.com>
2020-11-28 13:11:35 +0545

    first commit

---
+++ src/Teams.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import axios from 'axios';
+
+class Teams extends React.Component {
+
+    state = { teams: [] };
+    // const getTeams = async () => {
+    //     var teamsFound = this.props.teams;
+    //     var teamsArray = []
+    //     for(var key in teamsFound) {
+    //         var teamData = await axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamsFound[key].id}`)
+    //         teamsArray.push(teamData)
+    //     }
+    //     console.log(teamsArray)
+    //     return teamsArray;
+    // }
+    componentDidMount() {
+        this.onTeamsList();
+        console.log('mount')
+    }
+
+    onTeamsList = async () => {
+        const teamDataArray = [];
+        const teamsFound = this.props.teams;
+        for(var key in teamsFound) {
+            const response = await axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamsFound[key].id}`)
+            teamDataArray.push(response)
+        }
+
+        this.setState({ teams: teamDataArray })
+        console.log(this.state.teams)
+        
+    }
+
+
+    renderTeams() {
+       return this.state.teams.map(team => {
+            return (
+                    <div key={team.data.team.id}>
+                        <div>
+                            <h3>{team.data.team.displayName}</h3>
+                            <p>{team.data.team.standingSummary}</p>
+                        </div>
+                    </div>
+                )
+        });
+        
+        
+        // async () => {
+        //     for(var key in teamsFound) {
+        //         var teamData = await axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamsFound[key].id}`)
+        //         console.log(teamData)
+        //     }
+
+        //     return teamData;
+        //     // async function teamsData() {
+        //     //     var dataTeam = await axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamsFound[key].id}`)
+        //     //     console.log(dataTeam)
+        //     // }
+        //     // console.log(teamsData())         
+        // }
+        // return this.props.teams.map(async team => {
+        //     console.log(team)
+            // const teamData = await axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${team.id}`)
+            // console.log(teamData);
+            // return (
+            //     <div>
+            //         <div>
+            //             <h3>{teamData.data.team.displayName}</h3>
+            //             {teamData.data.team.standingSummary}
+            //         </div>
+            //     </div>
+            // )
+        // });
+    }
+
+    render() {
+        return (
+            <div>{this.renderTeams()}</div>
+        )
+    }
+}
+
+export default Teams;
